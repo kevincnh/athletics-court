2c4aa51cf2e2bfefbf79703e6302cb46b768053a
kevincnh <kevincnh@hotmail.com>
2026-06-23 18:27:07 +0800

    configure vite dev server proxy to Cloudflare Worker API

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,15 @@ export default defineConfig({
     },
   },
 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://athletics-court.kevincnh.workers.dev',
+        changeOrigin: true,
+      }
+    }
+  },
+
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
